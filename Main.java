b8573e22da3bef51aae4fba7505908ba3db92301
ShemeerCoder-01 <118423467+ShemeerCoder-01@users.noreply.github.com>
2023-01-20 11:30:00 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+	
+	public static void main(String[] args) {
+		  DeluxePizza dp = new DeluxePizza(false);
+//		    System.out.println(dp.getPrice());
+		    dp.addTakeaway();
+		    dp.addExtraCheese();
+		    dp.addTakeaway();
+		    System.out.println(dp.getBill());
+		    System.out.println();
+		    Pizza p = new Pizza(true);
+		    p.addExtraCheese();
+		    p.addExtraToppings();
+		    p.addTakeaway();
+		    System.out.println(p.getBill());
+		
+	}
+}
